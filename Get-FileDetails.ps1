c7318d28d3690c6d7f60b22420a1e336c7891234
Michał Malenda <michalmalenda@gmail.com>
2023-04-29 03:23:04 +0200

    Update Get-FileDetails.ps1

--- Get-FileDetails.ps1
+++ Get-FileDetails.ps1
@@ -1,11 +1,5 @@
 # Get the filename from the text file
-
 $filename = Get-Content "C:\Temp\in.txt"
-
- 
-
 # Check the last modification date of the file
-
 $lastModifiedDate = (Get-Item $filename).LastWriteTime
-
 $lastModifiedDate.ToShortDateString() | Out-File -FilePath "C:\Temp\out.txt"
